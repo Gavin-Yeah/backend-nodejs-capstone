f1b38b81e0c47a46c80f0028863e3f8141fb5a18
Gavin Ye <gavinjiadaye@gmail.com>
2024-06-26 17:33:06 -0400

    Update app.js

--- secondChance-backend/app.js
+++ secondChance-backend/app.js
@@ -11,7 +11,7 @@ const {loadData} = require("./util/import-mongo/index");
 const app = express();
 app.use("*",cors());
 const port = 3060;
-
+app.use(express.static('public'))
 // Connect to MongoDB; we just do this one time
 connectToDatabase().then(() => {
     pinoLogger.info('Connected to DB');
